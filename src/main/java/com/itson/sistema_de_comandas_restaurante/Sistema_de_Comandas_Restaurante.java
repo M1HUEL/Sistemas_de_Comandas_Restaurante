bfad63bcd688492467de85f5862626785f5f2472
M1HUEL <imiguelx008@gmail.com>
2025-10-31 16:23:43 -0600

    *

--- src/main/java/com/itson/sistema_de_comandas_restaurante/Sistema_de_Comandas_Restaurante.java
+++ src/main/java/com/itson/sistema_de_comandas_restaurante/Sistema_de_Comandas_Restaurante.java
@@ -1,20 +1,12 @@
 package com.itson.sistema_de_comandas_restaurante;
 
 import com.itson.sistema_de_comandas_restaurante.gui.Inicio;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class Sistema_de_Comandas_Restaurante {
-    
+
     public static void main(String[] args) {
-        
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            
-            new Inicio().setVisible(true);
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-            System.getLogger(Sistema_de_Comandas_Restaurante.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
-        }
-        
+
+        new Inicio().setVisible(true);
+
     }
 }
